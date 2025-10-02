d2a543fa0cee97ff0e123a24107479e46bdbad2c
SREEKRISH100 <krishnapvilasan124@gmail.com>
2025-10-02 15:20:39 +0530

    deploy commit

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ const dbConnect = async () => {
         await mongoose.connect(process.env.dbConnect)
         console.log('DB Connected')
     } catch (error) {
-        console.log('DB Server Error')
+        console.log('DB Server Error',error)
     }
 }
 dbConnect()
