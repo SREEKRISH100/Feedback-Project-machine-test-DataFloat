7fd4348e537999e497db26bdef37a5286a7429cd
SREEKRISH100 <krishnapvilasan124@gmail.com>
2025-10-02 11:31:27 +0530

    last and final commit

--- Server/model/Feedback.js
+++ Server/model/Feedback.js
@@ -1,22 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const feedbackSchema = mongoose.Schema({
-//     user: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User',
-//     },
-//     comments: { type: String, required: true },
-//     rating: {
-//         type: Number,
-//         min: 1,
-//         max: 5,
-//         required: true
-//     },
-// }, { timestamps: true })
-
-// const Feedback = mongoose.model('feedback_tbls', feedbackSchema);
-
-// module.exports = Feedback;
 const mongoose = require('mongoose');
 
 const feedbackSchema = mongoose.Schema({
